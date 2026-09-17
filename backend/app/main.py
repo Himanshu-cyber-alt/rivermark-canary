931159bda494d30f95ba48994e46213785e742ca
Himanshu Pagare <155243085+Himanshu-cyber-alt@users.noreply.github.com>
2026-09-17 13:52:41 +0530

    ch1

--- backend/app/main.py
+++ backend/app/main.py
@@ -21,7 +21,7 @@ def root():
         "application": "Rivermark Backend",
         "version": VERSION,
         "request_count": request_count,
-        "message": f"Hello from {VERSION}",
+        "message": f"Hello {VERSION}",
     }
 
 
