03bc9a599b44ee820028b5dc05d58d565982602d
Himanshu Pagare <155243085+Himanshu-cyber-alt@users.noreply.github.com>
2026-09-17 09:21:53 +0530

    Create React frontend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div>
+      <h1>Rivermark</h1>
+      <h2>Canary Deployment Demo</h2>
+      <p>Frontend Version V1</p>
+    </div>
+  );
+}
+
+export default App;
